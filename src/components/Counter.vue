c2d6e72b2db5b881fc6a45aa43c9400707ef89a0
Brandon Harris <brandon@relentnet.com>
2025-02-13 14:06:47 -0600

    Added a counter buttons component to learn about props.

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,24 +1,21 @@
 <script setup>
 import { ref } from 'vue'
+import CounterButtons from './CounterButtons.vue';
 
 const count = ref(0);
 
 const increment = () => count.value++;
 const decrement = () => count.value--;
+const reset = () => count.value = 0;
 </script>
 
 <template>
     <div class="bg-zinc-800 p-4 rounded shadow-md text-center">
         <h2 class="text-zinc-50 mb-3 text-2xl font-bold">Count: {{ count }}</h2>
         <div class="flex space-x-4">
-            <button @click="decrement"
-                class="p-4 bg-red-400 text-slate-950 rounded shadow-md hover:shadow-xl hover:bg-red-500 active:bg-red-600 active:shadow-3xl transition duration-300">
-                Decrement
-            </button>
-            <button @click="increment"
-                class="p-4 bg-green-400 text-slate-950 rounded shadow-md hover:shadow-xl hover:bg-green-500 active:bg-green-600 active:shadow-3xl transition duration-300">
-                Increment
-            </button>
+            <CounterButtons label="Decrement" :onClick="decrement" bgColor="bg-red-400" />
+            <CounterButtons label="Reset" :onClick="reset" bgColor="bg-yellow-400" />
+            <CounterButtons label="Increment" :onClick="increment" bgColor="bg-green-400" />
         </div>
     </div>
 </template>
---
+++ src/components/CounterButtons.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+    label: String,
+    onClick: Function,
+    bgColor: String
+});
+</script>
+
+<template>
+    <button @click="onClick"
+        class="p-4 text-slate-950 rounded shadow-md hover:shadow-xl active:shadow-3xl transition duration-300"
+        :class="[bgColor, 'hover:' + bgColor.replace('400', '500'), 'active:' + bgColor.replace('400', '600')]">
+        {{ label }}
+    </button>
+</template>
