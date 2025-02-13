929b871bf974052fd3eaa5c6e262d06140bb9c2a
Brandon Harris <brandon@relentnet.com>
2025-02-12 18:57:33 -0600

    Setup a simple counter component.

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Counter from './components/Counter.vue';
+</script>
+
+<template>
+  <div>
+    <h1 class="text-fuchsia-500">Vue 3 + Vite + TypeScript</h1>
+    <Counter />
+  </div>
+</template>
+
---
+++ src/components/Counter.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0);
+
+const increment = () => count.value++;
+const decrement = () => count.value--;
+</script>
+
+<template>
+    <div class="flex flex-col items-center space-y-4">
+        <h2>Count: {{ count }}</h2>
+        <div class="flex space-x-4">
+            <button @click="decrement"
+                class="p-4 bg-red-400 text-slate-950 rounded shadow-md hover:shadow-xl hover:bg-red-500 active:bg-red-600 active:shadow-3xl transition duration-200">
+                Decrement
+            </button>
+            <button @click="increment"
+                class="p-4 bg-green-400 text-slate-950 rounded shadow-md hover:shadow-xl hover:bg-green-500 active:bg-green-600 active:shadow-3xl transition duration-200">
+                Increment
+            </button>
+        </div>
+    </div>
+</template>
