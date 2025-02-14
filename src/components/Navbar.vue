59acf52a01f13662a951f048fae1b1b6afa907cc
Brandon Harris <brandon@relentnet.com>
2025-02-14 17:09:31 -0600

    Added a light theme and button to the navbar with local storage.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,13 @@
+<script setup>
+import Theme from "./Theme.vue";
+</script>
+
 <template>
-    <nav
-        class="bg-base text-text p-4 flex justify-center items-center fixed top-0 left-0 w-full space-x-4 font-semibold">
+    <nav class="bg-base text-text p-4 flex justify-end items-center fixed top-0 left-0 w-full space-x-2 font-semibold">
         <RouterLink to="/" activeClass="bg-surface"
-            class="hover:bg-surface rounded px-4 py-2 transition-colors duration-300">Home</RouterLink>
+            class="hover:bg-surface rounded-xl px-4 py-2 transition-colors duration-300">Home</RouterLink>
         <RouterLink to="/projects" activeClass="bg-surface"
-            class="hover:bg-surface rounded px-4 py-2 transition-colors duration-300">Projects</RouterLink>
+            class="hover:bg-surface rounded-xl px-4 py-2 transition-colors duration-300">Projects</RouterLink>
+        <Theme />
     </nav>
 </template>
---
+++ src/components/Theme.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { useColorMode } from '@vueuse/core'
+import { MoonIcon, SunIcon } from '@heroicons/vue/24/solid'
+
+const mode = useColorMode({
+    initialValue: 'dark',
+    storageKey: 'theme',
+    attribute: 'data-theme',
+});
+
+function toggleTheme() {
+    mode.value = mode.value === 'light' ? 'dark' : 'light';
+}
+</script>
+
+<template>
+    <div class="flex justify-end">
+        <button @click="toggleTheme"
+            class="px-4 py-2 rounded-xl cursor-pointer text-subtle hover:text-text hover:bg-surface transition duration-200">
+            <transition name="scale" mode="out-in">
+                <MoonIcon v-if="mode === 'dark'" class="size-6" key="moon" />
+                <SunIcon v-else key="sun" class="size-6" />
+            </transition>
+        </button>
+    </div>
+</template>
+
+<style scoped>
+.scale-enter-active,
+.scale-leave-active {
+    transition: transform 0.2s ease-in-out;
+}
+
+.scale-enter-from,
+.scale-leave-to {
+    transform: scale(0);
+}
+
+.scale-enter-to,
+.scale-leave-from {
+    transform: scale(1);
+}
+</style>
